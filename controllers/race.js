d69831d41d60fe55d98f5d53c4fa9c9a2e973a74
coralam17@gmail.com <coralam17@gmail.com>
2015-09-19 03:59:46 -0400

    Add race controller

---
+++ controllers/race.js
@@ -0,0 +1,16 @@
+var Race = require('../models/Race');
+
+exports.createRace = function (req, res) {
+  var race = new Race({
+    email: req.body.email,
+    start: {
+      time: req.body.startTime,
+      location: req.body.startLocation
+    }
+  });
+
+  race.save(function (err) {
+    if (err) return res.status(500);
+    return res.status(200);
+  });
+};
